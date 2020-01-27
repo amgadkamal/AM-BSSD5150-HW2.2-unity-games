32287338d9ba2b569af53491e7b98429e7ca0d2d
Amgad Morsy <42896629+amgadkamal@users.noreply.github.com>
2020-01-27 04:14:32 -0700

    hw2.2

---
+++ Assets/firstscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxSpeed = 10f;
+    void Start()
+    {
+        Debug.Log("Green Ball instantiated");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        float move2 = Input.GetAxis("Vertical");
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(move * maxSpeed, move2 * maxSpeed);
+       
+
+    }
+}
